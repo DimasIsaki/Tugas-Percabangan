349ba9917ca9bdc4a524adfbd282e725f8194488
DimasIsaki <samehadaposeidon@gmail.com>
2019-10-09 17:23:32 +0700

    Master

---
+++ Index.php
@@ -0,0 +1,10 @@
+<?php
+$a = 'Dimas Ramadhan Aji Kusuma.';
+$b = 'X RPL 1.';
+$c = '13.';
+echo "Perkenalkan nama saya $a";
+echo "<br>";
+echo "Saya kelas $b";
+echo "<br>";
+echo "Saya absen $c";
+?>
